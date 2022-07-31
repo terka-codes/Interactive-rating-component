7b2df157419d95668c4e708d5d85b5f34a582ce1
Terka Codes <107133029+terka-codes@users.noreply.github.com>
2022-07-31 20:38:54 +0200

    Overall update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const page = document.getElementById("page")
 const submitBtn = document.getElementById("submit-btn")
 const rateDiv = document.getElementById("rate")
 let rateBtns = rateDiv.getElementsByClassName("rate-btn")
+const selectRating = document.getElementById("select-rating")
 let selectedRating = ""
 
 for (let i = 0; i < rateBtns.length; i++) {
@@ -13,22 +14,20 @@ for (let i = 0; i < rateBtns.length; i++) {
 
         this.className += " active"
 
-        if (current.className === "rate-btn active") {
-            selectedRating = "3"
-        }
-
-        console.log(selectedRating)
+        selectedRating = this.value
         
+        submitBtn.addEventListener("click", function() {
+            page.innerHTML = `
+                <div id="thank-page">
+                    <img id="thank-img" src="./images/illustration-thank-you.svg" alt="thank you">
+                     <p id="user-rating">You selected ${selectedRating} out of 5</p>
+                     <h1>Thank you!</h1>
+                     <p id="thank-text" >We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+                </div>`
+        })
     })
 }
 
-
-submitBtn.addEventListener("click", function(e) {
-    page.innerHTML = `
-        <div id="thank-page">
-            <img id="thank-img" src="./images/illustration-thank-you.svg" alt="thank you">
-             <p id="user-rating">You selected ${selectedRating} out of 5</p>
-             <h1>Thank you!</h1>
-             <p id="thank-text" >We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
-        </div>`
+submitBtn.addEventListener("click", function() {
+    selectRating.innerHTML = `<p id="select-alert">*You have to select a rating</p>`
 })
